a1cf9e232603fbd53b8b744d28e7847936fb8e2e
fonkwe <williajeni@gmail.com>
2022-07-22 11:21:27 -0500

    Facebook-clone

---
+++ src/Header.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import FlagIcon from '@mui/icons-material/Flag';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import { Avatar, IconButton } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import ForumIcon from '@mui/icons-material/Forum';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+ function Header() {
+  return (
+    <div className="header">
+        <div className="header_left">
+            <img src="https://www.facebook.com/images/fb_icon_325x325.png" alt =""/>
+            <div className="header_input">
+               <SearchIcon />
+               <input placeHolder="Search Facebook" type="text" />
+            </div>
+            
+        </div>
+
+
+        <div className="header_center">
+            <div className="header_option">
+                <HomeIcon fontSize='large'/>
+            </div>
+            <div className="header_option">
+                <FlagIcon fontSize='large'/>
+            </div>
+            <div className="header_option">
+                <SubscriptionsIcon fontSize='large' />
+            </div>
+            <div className="header_option">
+                <StorefrontIcon  fontSize='large'/>
+            </div>
+            <div className="header_option">
+                <SupervisedUserCircleIcon fontSize='large' />
+            </div>
+        </div>
+
+        <div className="header_right">
+            <div className="header_info">
+                <Avatar />
+                <h4>Cliff Nji</h4>
+            </div>
+            <IconButton>
+                <AddIcon />
+            </IconButton>
+            <IconButton>
+                <ForumIcon />
+            </IconButton>
+            <IconButton>
+                <NotificationsActiveIcon />
+            </IconButton>
+            <IconButton>
+                <ExpandMoreIcon />
+            </IconButton>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAi6F327kmZc8daYKX5ro4605nmImEci2E",
+    authDomain: "facebook-clone-63256.firebaseapp.com",
+    projectId: "facebook-clone-63256",
+    storageBucket: "facebook-clone-63256.appspot.com",
+    messagingSenderId: "1020077206791",
+    appId: "1:1020077206791:web:66607fb1cc529882f1e9a2",
+    measurementId: "G-B6B0WKVS94"
+  };
